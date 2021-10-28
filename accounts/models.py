c70aa5f6439736a4669eae05747bd771b1c218ac
Florian Delage <delageflo@ymail.com>
2021-10-28 23:17:14 +0200

    update accounts/test_models.py

--- accounts/models.py
+++ accounts/models.py
@@ -5,10 +5,11 @@ from django.contrib.auth.models import AbstractUser
 
 class Profile(AbstractUser):
     email = models.EmailField(unique=True, error_messages={'unique': 'Un utilisateur avec cet email existe déjà.'}, max_length=254)
+    username = models.CharField(unique=True, error_messages={'unique': 'Un utilisateur avec ce nom existe déjà.'}, max_length=128)
     password = models.CharField(max_length=128)
 
     REQUIRED_FIELDS = ['email','password'] # required fields at the creation
     USERNAME_FIELD = 'username' # set as login field / by the way set as required field implicitly
 
     def __str__(self):
-        return f"Profil {self.pk}: {self.email}"
+        return f"Profil {self.pk}: {self.username} / {self.email}"
--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -10,7 +10,7 @@ class ProfileModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         # Set up non-modified objects used by all test methods
-        Profile.objects.create(email='guido@django.com', password='Django1234')
+        Profile.objects.create(username='guido', email='guido@django.com', password='Django1234')
 
     def test_email(self):
         profile = Profile.objects.get(id=1)
@@ -25,12 +25,32 @@ class ProfileModelTest(TestCase):
     def test_email_error_messages_unique(self):
         profile = Profile.objects.get(id=1)
         error_messages_unique = profile._meta.get_field('email').error_messages['unique']
-        self.assertEquals(error_messages_unique, 'A profile with that email already exists.')
+        self.assertEquals(error_messages_unique, 'Un utilisateur avec cet email existe déjà.')
 
     def test_email_max_length(self):
         profile = Profile.objects.get(id=1)
         max_length = profile._meta.get_field('email').max_length
         self.assertEquals(max_length, 254)
+
+    def test_username(self):
+        profile = Profile.objects.get(id=1)
+        username = profile.username
+        self.assertEquals(username, 'guido')
+
+    def test_username_unique(self):
+        profile = Profile.objects.get(id=1)
+        unique = profile._meta.get_field('username').unique
+        self.assertEquals(unique, True)
+
+    def test_username_error_messages_unique(self):
+        profile = Profile.objects.get(id=1)
+        error_messages_unique = profile._meta.get_field('username').error_messages['unique']
+        self.assertEquals(error_messages_unique, 'Un utilisateur avec ce nom existe déjà.')
+
+    def test_username_max_length(self):
+        profile = Profile.objects.get(id=1)
+        max_length = profile._meta.get_field('username').max_length
+        self.assertEquals(max_length, 128)
 
     def test_password(self):
         profile = Profile.objects.get(id=1)
@@ -42,7 +62,17 @@ class ProfileModelTest(TestCase):
         max_length = profile._meta.get_field('password').max_length
         self.assertEquals(max_length, 128)
 
+    def test_required_fields(self):
+        profile = Profile.objects.get(id=1)
+        required_fields = profile.REQUIRED_FIELDS
+        self.assertEquals(required_fields, ['email', 'password'])
+
+    def test_username_field(self):
+        profile = Profile.objects.get(id=1)
+        username_field = profile.USERNAME_FIELD
+        self.assertEquals(username_field, 'username')
+
     def test_object_str(self):
         profile = Profile.objects.get(id=1)
         object_str = profile.__str__()
-        self.assertEquals(object_str, f"Profil {profile.pk}: {profile.email}")
+        self.assertEquals(object_str, f"Profil {profile.pk}: {profile.username} / {profile.email}")
