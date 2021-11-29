64ae3b23d2e3d6f1a873a51430ebef5f4e72112a
Florian Delage <delageflo@ymail.com>
2021-11-29 11:27:21 +0100

    sljbqzfl

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'livereload',
     'django.contrib.staticfiles',
 
     'catalog',
@@ -66,7 +65,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'pur_beurre.urls'
