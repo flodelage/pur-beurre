5d2205d13cff4964460438ab487b6f7a0f444e15
Florian Delage <delageflo@ymail.com>
2021-12-01 14:05:49 +0100

    dotenv

--- manage.py
+++ manage.py
@@ -2,11 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import os
+
+import dotenv
 
 
 def main():
     """Run administrative tasks."""
+    dotenv.read_dotenv()
+    print(os.environ.get('ENV'))
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pur_beurre.settings')
     try:
         from django.core.management import execute_from_command_line
--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -15,11 +15,6 @@ from pathlib import Path
 from django.contrib.messages import constants as messages
 
 import django_heroku
-import environ
-
-# Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
 
 
 MESSAGE_TAGS = {
@@ -38,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_KEY', default='django-insecure-qy&-a_535y62ii7^yl%o_in$eqg-j$r8&s)htz9a^u3vjk428n')
+SECRET_KEY = os.environ.get('DJANGO_KEY', default='django-insecure-qy&-a_535y62ii7^yl%o_in$eqg-j$r8&s)htz9a^u3vjk428n')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if os.environ.get('ENV', 'development') == 'production' else True
 
@@ -96,9 +91,9 @@ WSGI_APPLICATION = 'pur_beurre.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': '',
         'PORT': '5432',
     }
