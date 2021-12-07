9bc944093acc144b9d9a8687db2598385f1a0b50
Florian Delage <delageflo@ymail.com>
2021-12-07 17:52:03 +0100

    wrote docstring in context_processors.py

--- catalog/context_processors.py
+++ catalog/context_processors.py
@@ -3,5 +3,8 @@ from .forms import NavSearchForm
 
 
 def nav_bar_form(request):
+    """
+    Add navbar form to all contexts
+    """
     navbar_form = NavSearchForm()
     return {'navbar_form': navbar_form}
