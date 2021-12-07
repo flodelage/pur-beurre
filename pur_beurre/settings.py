2ca299b4b3706526e1a298d222ddc527f5630037
Florian Delage <delageflo@ymail.com>
2021-12-07 11:54:12 +0100

    test heroku

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -36,8 +36,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('ENV', 'development') != 'production'
-
+# DEBUG = os.environ.get('ENV', 'development') != 'production'
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
